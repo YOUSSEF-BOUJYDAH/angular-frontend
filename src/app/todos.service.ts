e93f3e3ece898d5eb242e0fcc70a4f8d243d01b0
YOUSSEF-BOUJYDAH <youssef.boujydah@ump.ac.ma>
2022-11-13 00:14:54 +0100

    error fixed agian

--- src/app/todos.service.ts
+++ src/app/todos.service.ts
@@ -11,6 +11,6 @@ export class TodosService {
 
 
   getAllFilieres(): Observable<any[]> {//pour travailler en mode asynchrone
-    return this.http.get<any[]>("  http://192.168.49.2:32666/api/todos");//of(x) return an observable for the value passed in arguments
+    return this.http.get<any[]>("http://192.168.49.2:32666/api/todos");//of(x) return an observable for the value passed in arguments
   }
 }
